1832e8b34801e1838b0d657047f1be130eb050ba
fyriz20-design <fyriz20@gmail.com>
2026-05-13 20:31:27 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,3 @@
-export default function Home() { return ( <div style={{padding:"50px", textAlign:"center"}}> <h1>Berndos Flohmarkt</h1> <p>Die Seite ist online.</p> <a href="/admin">Zum Admin-Bereich</a> </div> ); }
+export default function Home() {
+  return <div>Berndos Flohmarkt Online</div>;
+}
